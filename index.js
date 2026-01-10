a12c26c50ca06e1d2635a09c732192acac144fe7
marielav1043 <139282125+marivalerio104@users.noreply.github.com>
2026-01-09 20:36:55 -0600

    Add joke fetching functionality from API

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 import express from "express";
+import axios from "axios";
 
-const app = express();
 const port = 3000;
+const app = express();
 
-app.listen(port, () => {console.log(`Server running on port ${port}`)});
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`)
+});
 
 app.get("/", (req, res) => {
   res.render("index.ejs")
+});
+
+app.post("/", async (req, res) => {
+  try {
+    const response = await axios.get(`https://v2.jokeapi.dev/joke/Programming?type=${req.body.type}`);
+    res.render("index.ejs", {joke: response.data})
+  } catch (error) {
+    console.error("Failed to make request: " + error.message);
+    res.status(500).send("Failed to fetch joke. Please try again.");
+  }
 });
